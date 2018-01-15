22847370c1446c531a9ba6a8da095d34655e22ad
Unknown <binickname@gmail.com>
2018-01-15 09:56:24 +0900

    Update parameter resolutions based on latest version of Junit 5.0.2

--- junit5-parameter-resolution-example/src/test/java/xyz/howtoprogram/junit5/order/TestOrderService.java
+++ junit5-parameter-resolution-example/src/test/java/xyz/howtoprogram/junit5/order/TestOrderService.java
@@ -1,47 +1,44 @@
 package xyz.howtoprogram.junit5.order;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.api.TestReporter;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.platform.runner.JUnitPlatform;
-import org.junit.runner.RunWith;
 
-@RunWith(JUnitPlatform.class)
 @ExtendWith(ReportAnnotationParameterResolver.class)
 @ExtendWith(AuditServiceParameterResolver.class)
-//@ExtendWith(EnvironmentNameParameterResolver.class)
+// @ExtendWith(EnvironmentNameParameterResolver.class)
 public class TestOrderService {
 
-	@DisplayName("Test placeOrder method")
-	@Test
-	public void placeOrderOK(TestInfo testInfo, TestReporter testReporter) {
+  @DisplayName("Test placeOrder method")
+  @Test
+  public void placeOrderOK(TestInfo testInfo, TestReporter testReporter) {
 
-		assertEquals("Test placeOrder method", testInfo.getDisplayName());
-		testReporter.publishEntry("key", "value");
+    assertEquals("Test placeOrder method", testInfo.getDisplayName());
+    testReporter.publishEntry("key", "value");
 
-		assertEquals(1, 1);
-	}
+    assertEquals(1, 1);
+  }
 
-	@Test
-	public void placeOrderNoItem(AuditService auditService) {
-		auditService.audit();
-		assertEquals(1, 1);
-	}
+  @Test
+  public void placeOrderNoItem(AuditService auditService) {
+    auditService.audit();
+    assertEquals(1, 1);
+  }
 
-	@Test
-	public void placeOrderNG(@Report ReportService reportService) {
-		reportService.report();
-		assertEquals(1, 1);
-	}
+  @Test
+  public void placeOrderNG(@Report ReportService reportService) {
+    reportService.report();
+    assertEquals(1, 1);
+  }
 
-//	@Test
-//	public void placeOrderNotLogin(EnvironmentInfo env) {
-//		assertEquals("Windows 10", env.getOS());
-//		assertEquals(1, 1);
-//	}
+  // @Test
+  public void placeOrderNotLogin(EnvironmentInfo environmentInfo) {
+    System.out.println(environmentInfo.getOS());
+    assertEquals("Windows 10", environmentInfo.getOS());
+    assertEquals(1, 1);
+  }
 
 }
