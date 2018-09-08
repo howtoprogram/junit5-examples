8eb8a4f1d7b4a806e1a94a964362b35a2b6e77d3
Unknown <binickname@gmail.com>
2018-09-08 20:44:19 +0700

    Upgraded to JUnit 5.3.0

--- junit5-assertions-examples/src/test/java/com/howtoprogram/junit5/StringUtilsTestUnit5.java
+++ junit5-assertions-examples/src/test/java/com/howtoprogram/junit5/StringUtilsTestUnit5.java
@@ -4,10 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.junit.platform.runner.JUnitPlatform;
-import org.junit.runner.RunWith;
-@Disabled
-@RunWith(JUnitPlatform.class)
+
 public class StringUtilsTestUnit5 {
 
   @Test
