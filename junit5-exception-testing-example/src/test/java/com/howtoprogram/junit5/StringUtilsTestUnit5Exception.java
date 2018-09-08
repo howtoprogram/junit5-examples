f414f4935d69a67e4d2e6d9421da59e757cc71ed
Unknown <binickname@gmail.com>
2018-09-08 21:29:22 +0700

    Upgraded to JUnit 5.3.0

--- junit5-exception-testing-example/src/test/java/com/howtoprogram/junit5/StringUtilsTestUnit5Exception.java
+++ junit5-exception-testing-example/src/test/java/com/howtoprogram/junit5/StringUtilsTestUnit5Exception.java
@@ -4,10 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.Test;
-import org.junit.platform.runner.JUnitPlatform;
-import org.junit.runner.RunWith;
-
-@RunWith(JUnitPlatform.class)
 
 public class StringUtilsTestUnit5Exception {
 
